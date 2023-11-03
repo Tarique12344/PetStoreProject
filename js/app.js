781c587fca8fd74dd59e3b891801b55923b7b188
Tarique12344 <Tariqueelshamy11@gmail.com>
2023-11-03 06:58:32 -0400

    I Changed the banners for each page

---
+++ js/app.js
@@ -0,0 +1,26 @@
+/*!
+* Start Bootstrap - Shop Homepage v5.0.6 (https://startbootstrap.com/template/shop-homepage)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-homepage/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+
+
+// document.getElementById("demo").style.cursor = "pointer";
+
+// const cursorSmall = document.querySelector('.small');
+// const cursorBig = document.querySelector('.big');
+
+
+// const positionElement = (e)=> {
+//   const mouseY = e.clientY;
+//   const mouseX = e.clientX;
+   
+//   cursorSmall.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+  
+//   cursorBig.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+ 
+// }
+
+// window.addEventListener('mousemove', positionElement)
